8b0f03bb83ae873100a4f77700f81c01c283e54a
abirhasan01 <appleabirhasan@gmail.com>
2025-12-20 09:42:15 -0800

    upading file

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react"
 import ShopContext from "../context/shopContext"
+import ProductItem from "./ProductItem"
 import Title from "./Title"
 
 const LatestCollection = () => {
@@ -16,12 +17,26 @@ const LatestCollection = () => {
     <div className="my-10">
       <div className="text-center py-8 text-3xl">
         <Title text1={"LATEST"} text2={"COLLECTIONS"} />
-        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, iusto.</p>
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta,
+          iusto.
+        </p>
       </div>
 
-    
+      {/* Rendering products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default LatestCollection
--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -1,10 +1,26 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import ShopContext from "../context/shopContext";
 
-const ProductItem = ({id, img, name, price}) => {
+const ProductItem = ({ id, image, name, price }) => {
+  const { currency } = useContext(ShopContext);
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
+      <div className="overflow-hidden">
+        <img
+          className="hover:scale-110 transition ease-in-out"
+          src={image[0]}
+          alt=""
+        />
+      </div>
+      <p className="pt-3 pb-1 text-sm">{name}</p>
+      <p className="text-sm font-medium">
+        {currency}
+        {price}
+      </p>
+    </Link>
+  );
+};
 
 export default ProductItem
